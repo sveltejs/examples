cf24446af1d80dc6a64f7a2e242ff842686ced50
Karim Jordan <karim@winter-rose.design>
2023-02-28 08:54:28 +0000

    basic layout and global stylesheet

---
+++ examples/sveltekit-file-uploads-nodejs/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '$lib/css/styles.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		margin-block-start: 3rem;
+		padding: 1rem;
+	}
+
+	:global(main > *) {
+		max-width: 60rem;
+		margin-inline: auto;
+	}
+</style>
--- examples/sveltekit-file-uploads-nodejs/src/routes/+page.svelte
+++ examples/sveltekit-file-uploads-nodejs/src/routes/+page.svelte
@@ -1,2 +1,108 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { enhance } from '$app/forms';
+	import { slide } from 'svelte/transition';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+	/** @type {import('./$types').ActionData} */
+	export let form;
+</script>
+
+<h1>Upload and view files with SvelteKit and Node.js</h1>
+
+<div class="forms">
+	<form class="small" method="POST" action="?/upload" enctype="multipart/form-data" use:enhance>
+		<div>
+			<label for="file">Select a small file</label>
+			<input type="file" name="file" id="file" required />
+		</div>
+		<button>Upload</button>
+	</form>
+
+	<form class="large">
+		<div>
+			<label for="file">Select a large file</label>
+			<input type="file" name="file" id="file" required />
+		</div>
+		<button>Upload</button>
+	</form>
+</div>
+
+<section aria-label="Uploaded files">
+	<h2>Uploaded files</h2>
+	{#if !data?.files.length}
+		<p>No files have been uploaded yet.</p>
+	{:else}
+		<ol class="files">
+			{#each data.files as file (file.name)}
+				<li class="file" transition:slide={{ duration: 200 }}>
+					<span class="file__size">{(file.size / 1000).toFixed(1)} kB</span>
+					<a class="file__name" href="/files/{file.name}" target="_blank" rel="noreferrer">
+						{file.name}
+					</a>
+					<form
+						class="file__actions"
+						use:enhance
+						method="POST"
+						action="?/delete"
+						enctype="multipart/form-data"
+					>
+						<button name="file" value={file.name}>Delete</button>
+					</form>
+				</li>
+			{/each}
+		</ol>
+	{/if}
+</section>
+
+<style>
+	.forms {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+		margin-block-end: 2rem;
+	}
+
+	.forms > form {
+		padding: 1rem;
+		border-radius: var(--border-radius);
+		background-color: hsla(0, 0%, 0%, 0.07);
+		flex: 1 0 20rem;
+	}
+
+	.forms > form > button {
+		margin-block-start: 1rem;
+	}
+
+	.files {
+		padding: 0;
+	}
+
+	.file {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		padding: 0.5rem;
+		border-radius: var(--border-radius);
+		font-size: 1.2rem;
+
+		transition: background-color, 100ms;
+	}
+
+	.file:hover {
+		background-color: hsla(0, 0%, 0%, 0.07);
+	}
+
+	.file__size {
+		flex: 0 0 15ch;
+		font-family: monospace;
+	}
+
+	.file__name {
+		flex: 1 0 auto;
+	}
+
+	.file__actions {
+		flex: 0 0 auto;
+	}
+</style>
