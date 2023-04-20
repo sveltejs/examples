59d6c28398ddf4705718290f9ebab657df1092a6
Karim Jordan <karim@winter-rose.design>
2023-04-20 04:23:39 +0100

    update store

--- examples/sveltekit-sse/src/lib/sse-store.js
+++ examples/sveltekit-sse/src/lib/sse-store.js
@@ -1,54 +1,51 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
-function create_sse_store() {
+function create_sse_store(init = false) {
 	/** @type {EventSource | undefined} */
 	let event_source;
 
 	const { subscribe, update } = writable({ clients: [], messages: [], status: 'closed' }, () => {
-		if (browser) {
-			connect();
-		}
+		if (init) connect();
 
 		return close;
 	});
 
+	function on_error(e) {
+		update((value) => ({ ...value, status: 'error' }));
+	}
+	function on_open(e) {
+		update((value) => ({ ...value, status: 'open' }));
+	}
+	function on_client_list(e) {
+		const json = JSON.parse(e.data);
+		update((value) => ({ ...value, ...json }));
+	}
+	function on_client_message(e) {
+		const json = JSON.parse(e.data);
+	
+		update((value) => {
+			value.messages.push(json);
+			return value;
+		});
+	}
+
 	function connect() {
 		event_source = new EventSource('/sse');
 
-		event_source.addEventListener('error', (event) => {
-			console.info('error')
-			update((value) => ({ ...value, status: 'error' }));
-		});
-
-		event_source.addEventListener('open', (event) => {
-			console.info('open')
-
-			update((value) => ({ ...value, status: 'open' }));
-		});
-
-		event_source.addEventListener('client:list', (event) => {
-			console.info('client:list')
-
-			const json = JSON.parse(event.data);
-
-			update((value) => ({ ...value, ...json }));
-		});
-
-		event_source.addEventListener('client:message', (event) => {
-			console.info('client:message')
-
-			const json = JSON.parse(event.data);
-
-			update((value) => {
-				value.messages.push(json);
-				return value;
-			});
-		});
+		event_source.addEventListener('error', on_error);
+		event_source.addEventListener('open', on_open);
+		event_source.addEventListener('client:list', on_client_list);
+		event_source.addEventListener('client:message', on_client_message);
 	}
 
 	function close() {
 		event_source?.close();
+
+		event_source?.removeEventListener('error', on_error);
+		event_source?.removeEventListener('open', on_open);
+		event_source?.removeEventListener('client:list', on_client_list);
+		event_source?.removeEventListener('client:message', on_client_message);
 	}
 
 	return {
@@ -56,6 +53,6 @@ function create_sse_store() {
 	};
 }
 
-const sse_store = create_sse_store();
+const sse_store = create_sse_store(browser);
 
 export default sse_store;
